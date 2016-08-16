77c0254b433e4efeff651e76cea10428e9ea12fa
Ryan Workman <rdavid1099@gmail.com>
2016-08-16 10:57:27 -0600

    Basic formatting

--- app/controllers/event_manager_app.rb
+++ app/controllers/event_manager_app.rb
@@ -3,6 +3,10 @@ require 'models/event_manager'
 class EventManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  get '/' do
+    redirect '/events'
+  end
+
   get '/events' do
     @events = event_manager.all
     erb :index
